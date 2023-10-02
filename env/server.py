7953e432d0439598fb756533a81db77fcd4762bb
Jose Rojo <jrborquez@hotmail.com>
2023-10-02 04:00:53 -0700

    bd

--- env/server.py
+++ env/server.py
@@ -9,18 +9,31 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def introducePedido():
     encabezado = 'Bienvenido a Burger Bros'
     titulo = 'Burger Bros'
+    if request.method == 'GET':
+        if request.form['id'] == post_id:
+            ()
+    post_id = request.form.get('id')
+    
     return render_template('index.html', encabezado=encabezado, titulo=titulo)
 
 
 
 @app.route('/infoPedido')
 def imprimirInfo():
+   @app.route('/', methods=["GET","POST"])
+   def home():    
+    if request.method == 'POST':
+        if request.form['delete'] == post_id:
+            (my sql login/cursor stuff....)
+            sql = "DELETE FROM test_table WHERE post_id = ?"
+            c.execute(sql, (post_id,))
+            return redirect("/")
+
+    
+
     return render_template('infoPedido.html')
-
-
-
 
